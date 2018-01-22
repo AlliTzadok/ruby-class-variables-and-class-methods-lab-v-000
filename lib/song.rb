d195d5c949b58c9d4db21f48a89016226109f064
Alli Tzadok <allisontzadok@gmail.com>
2018-01-22 20:55:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  @@name = ""
-  @@artist = ""
-  @@genre = ""
+  @@count = 0
 
-  def initialize(name, artist, genre)
+  def initialize
+    @@count += 1
+  end
+
+  def self.count
+    @@count
   end
 
 end
