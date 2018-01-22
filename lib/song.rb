53d6e6b5fdeddb1553e1f545ed6845d06b5a4f50
Alli Tzadok <allisontzadok@gmail.com>
2018-01-22 21:24:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+
   @@count = 0
   @@artists = []
   @@genres = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -13,26 +13,24 @@ class Song
     @@artists << artist
     @@genres << genre
   end
-  
-  
+
+
   def self.count
     @@count
   end
-  
+
   def self.genres
     @@genres.uniq
   end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
+
   def self.genre_count
     genre_count = {}
     @@genres.each do {|genre| genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1}
     end
     genre_count
   end
-
 end
-
